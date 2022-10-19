42fb17d91115701576061262fac10a4aea28908d
jefrete <74308020+jefrete@users.noreply.github.com>
2022-10-19 12:58:03 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,6 +14,79 @@ background(0);
 
 function draw () {
  
-let img = loadImage('harold.png');
+ 
+let bellasA;
+bellasA=random(0,255);
+
+function setup () {
+
+createCanvas(1024,600);
+background(0);
+//line(15, 25, 70, 90);
+
+}
+
+function draw () {
+    
+
+fill(bellasA,bellasA,random(0,255));
+text("HIDE YOUR PAIN", random(0,1024),random(0,600));
+
+
+//epilepsia    
+//background(random(0,255),random(0,255),random(0,255));
+
+fill(random(0,255),random(0,255),random(0,255));
+    ellipse(random(0,width),random(0,height),50);     
+   
+/*fill(random(0,255),random(0,255),random(0,255));
+ellipse(random(0,1024),random(0,600),random(0,100));
+*/
+    
+/*fill(random(0,255),random(0,255),random(0,255));
+ellipse(random(0,1024),random(0,600),50);
+*/
+    
+/*    
+stroke(0,255,0);
+strokeWeight(5);
+//noFill();
+line(pmouseX, pmouseY, mouseX, mouseY);
+*/
+ 
+    
+//HAROLD    
+
+/*let img = createImg("https://s3.getstickerpack.com/storage/uploads/sticker-pack/hide-the-pain-harold/sticker_5.png?35bc9a5413d14b83fb1eabdb6fe2523d&d=200x200");
+
 img.position(random(0,1024),random(0,600));
+*/
+    
+    
+/*
+fill(0,255,0)    
+rect(mouseX,mouseY,50,50)    
+*/
+    
+    
+    
+/*let x = mouseX;
+  let y = mouseY;
+  let ix = width - mouseX;  // Inverse X
+  let iy = height - mouseY; // Inverse Y
+  background(126);
+  fill(255, 150);
+  ellipse(x, height/2, y, y);
+  fill(0, 159);
+  ellipse(ix, height/2, iy, iy);  
+*/
+    
+/*fill(255);
+ellipse (100,100,50);
+ 
+ellipse(x, height/2, 20, 20);
+x = x + 1;
+        
+fill(255);
+rect(x,y,50,50);
 }
