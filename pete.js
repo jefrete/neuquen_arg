71190182e8138eddc223e39104630e9df69c6da0
jefrete <74308020+jefrete@users.noreply.github.com>
2023-05-05 13:01:27 -0300

    Add files via upload

---
+++ pete.js
@@ -0,0 +1,63 @@
+lista = [
+    'tres_sesenta',
+    'twitch_siempre',
+    'tchpro',
+    'techtrend',
+    'clubamigosdelaweb',
+    'clickpark',
+    'papi_tvg',
+    'sp_godlatino',
+    'ranmaseries'
+]
+
+var canal = 0;
+var player = new Twitch.Player("culo2", options);
+function cambiarCanal(rrrr) {
+    var canal = rrrr;
+    player.destroy();
+    options.channel = canal;
+    player = new Twitch.Player("culo2", options);
+}
+
+function mutear() {
+    player.destroy();
+    player = new Twitch.Player("culo2", options);
+
+}
+
+function cambioma() {
+    canal++;
+    if (canal > lista.length-1) {
+        canal = 0;
+    }
+    options.channel = lista[canal];
+    player.destroy();
+
+    player = new Twitch.Player("culo2", options);
+}
+function cambiome() {
+    canal--;
+    if (canal < 0) {
+        canal = lista.length-1;
+    }
+    options.channel = lista[canal];
+
+    player.destroy();
+
+    player = new Twitch.Player("culo2", options);
+}
+  /*function canalArriba() {
+          if{
+              sopa=request
+              sopita=soup(sopa)
+              sopita=select(div,class='canal')
+              objetivo=canal apagado
+          }else{
+              options.channel = lista[1++]
+          }
+          player.destroy();
+          player = new Twitch.Player("culo2", options);
+      }
+      ano();
+      */
+
