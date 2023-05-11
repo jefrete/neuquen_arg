bcee38b62be096f023613d59e43ae379c144f73c
jefrete <74308020+jefrete@users.noreply.github.com>
2023-05-11 03:43:58 -0300

    Add files via upload

---
+++ tvsur.js
@@ -0,0 +1,62 @@
+lista = [
+    'entretenimientomasivo',
+    'tchpro',
+    'clubamigosdelaweb',
+    'twitch_siempre',
+    'ranmaseries',
+    'ovin00',
+    'sp_godlatino',
+    'saintobronze40',
+    'techtrend'
+]
+
+var canal = 0;
+var player = new Twitch.Player("culo2", options);
+function cambiarCanal(rrrr) {
+    var canal = rrrr;
+    player.destroy();
+    options.channel = canal;
+    player = new Twitch.Player("culo2", options);
+}
+
+function mutear() {
+    player.destroy();
+    player = new Twitch.Player("culo2", options);
+
+}
+
+function cambioma() {
+    canal++;
+    if (canal > lista.length-1) {
+        canal = 0;
+    }
+    options.channel = lista[canal];
+    player.destroy();
+
+    player = new Twitch.Player("culo2", options);
+}
+function cambiome() {
+    canal--;
+    if (canal < 0) {
+        canal = lista.length-1;
+    }
+    options.channel = lista[canal];
+
+    player.destroy();
+
+    player = new Twitch.Player("culo2", options);
+}
+  /*function canalArriba() {
+          if{
+              sopa=request
+              sopita=soup(sopa)
+              sopita=select(div,class='canal')
+              objetivo=canal apagado
+          }else{
+              options.channel = lista[1++]
+          }
+          player.destroy();
+          player = new Twitch.Player("culo2", options);
+      }
+      ano();
+      */
